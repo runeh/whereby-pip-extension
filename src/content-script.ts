fee3f8f50a338179bea10df8e7deb134c11be985
Rune Finstad Halvorsen <runefh@gmail.com>
2020-04-22 23:06:51 +0200

    wip

--- src/content-script.ts
+++ src/content-script.ts
@@ -10,7 +10,6 @@ interface PipState {
 const isDev = process.env.NODE_ENV === 'development';
 
 let showPip = false;
-let initialized = false;
 
 function initMediaPipState(): PipState {
   const canvas = document.createElement('canvas');
@@ -20,14 +19,13 @@ function initMediaPipState(): PipState {
   // 1280x720
   // 640x 360
   canvas.width = 1024;
-  canvas.height = 800;
+  canvas.height = 576;
 
   pipVideo.muted = true;
   pipVideo.autoplay = true;
   pipVideo.srcObject = canvas.captureStream();
 
   const sources = document.getElementsByTagName('video');
-
   return { canvas, context, pipVideo, sources };
 }
 
@@ -56,15 +54,14 @@ function tick(state: PipState) {
     }),
   );
 
-  // console.log(JSON.stringify(dims, null, 2));
   if (dims.some(Number.isNaN)) {
     return;
   }
 
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.strokeStyle = '#000000';
-  eles.forEach((video, _index) => {
-    const { left, top, height, width } = dims[_index];
+  eles.forEach((video, index) => {
+    const { left, top, height, width } = dims[index];
     context.drawImage(video, left, top, width, height);
     context.strokeRect(left, top, width, height);
   });
@@ -76,19 +73,19 @@ function sleep(ms: number) {
 
 async function waitForConnectedRoom() {
   while (document.querySelector('[class|=ConnectedRoom]') == null) {
-    await sleep(2000);
+    await sleep(200);
   }
 }
 
 async function mainLoop(state: PipState) {
   while (showPip) {
-    console.log('vidstate3', currentState.pipVideo.readyState);
     tick(state);
     await sleep(33);
   }
 }
 
 async function initExtension() {
+  // fixme: remove / move this
   await waitForConnectedRoom();
   const state = initMediaPipState();
   const { pipVideo } = state;
@@ -118,11 +115,10 @@ let currentState: PipState | undefined;
 async function main() {
   showPip = !showPip;
 
-  if (showPip) {
-    currentState = await initExtension();
-    console.log('vidstate1', currentState.pipVideo.readyState);
+  currentState = currentState || (await initExtension());
 
-    await tick(currentState);
+  if (showPip) {
+    tick(currentState);
     await videoReady(currentState);
     await currentState.pipVideo.requestPictureInPicture();
     await mainLoop(currentState);
@@ -134,7 +130,6 @@ async function main() {
 }
 
 chrome.runtime.onMessage.addListener((message) => {
-  console.log('got a message in content script1234', message);
   main();
 });
 
